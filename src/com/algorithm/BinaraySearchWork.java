32e1db263994251b1acd8b310ad4355d2c88c3aa
Gaowtam Barua <128220812+Gaowtam@users.noreply.github.com>
2024-05-26 17:14:02 +0600

    Update BinaraySearchWork.java

--- src/com/algorithm/BinaraySearchWork.java
+++ src/com/algorithm/BinaraySearchWork.java
@@ -9,7 +9,9 @@ public class BinaraySearchWork {
 		int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
         int target = 16;
         int result = binarySearch(arr, target);
-        System.out.println("Element found at index " + result);
+        System.out.println("Element found at index Withoout Recursion : " + result);
+        int result2 = binarySearchRecursion(arr, target);
+        System.out.println("Element found at index With Recursion : " + result2);
 	}
 	
 	public  int binarySearch(int[] arr, int target) {
@@ -18,6 +20,29 @@ public class BinaraySearchWork {
         
         while (left <= right) {
             int mid = right + left  / 2;
+            
+            if (arr[mid] == target)
+            {
+            	return mid;
+            }
+            if (arr[mid] < target)
+            {
+            	left = mid + 1;
+            }
+            else
+            {
+            	right = mid - 1;
+            }
+        }
+        
+        return -1;
+	}
+	public  int binarySearchRecursion(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left <= right) {
+            int mid = left +(right-left)  / 2;
             
             if (arr[mid] == target)
             {
